db6fba290a5400639874f0720c457c27cd95baca
Ki Sung Song <sksstar@koscom.co.kr>
2023-06-16 16:11:15 +0900

    Modify name and email in pr_practice.py

--- results/sksstar/src/pr_practice.py
+++ results/sksstar/src/pr_practice.py
@@ -5,5 +5,5 @@
 # print("My name is Hong gil-dong")
 # print("Please contact me at gd.hong@email.com")
 
-print("My name is <NAME>")
-print("Please contact me at <EMAIL>")
+print("My name is sksstar")
+print("Please contact me at sksstar@koscom.co.kr")
