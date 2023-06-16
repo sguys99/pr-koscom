033029dffbb08df65206ac1af31fc626dfdc90af
shha0511 <shha0511@naver.com>
2023-06-16 16:11:45 +0900

    Modify name and email in pr_practice.py

--- results/shha0511/src/pr_practice.py
+++ results/shha0511/src/pr_practice.py
@@ -5,5 +5,5 @@
 # print("My name is Hong gil-dong")
 # print("Please contact me at gd.hong@email.com")
 
-print("My name is <NAME>")
-print("Please contact me at <EMAIL>")
+print("My name is Sanghoon Ha")
+print("Please contact me at shha0511@naver.com")
